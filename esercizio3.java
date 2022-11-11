05f20218c130bb117253aab6ec6100becc7082b8
AlessandroPal <117976138+AlessandroPal@users.noreply.github.com>
2022-11-11 11:12:56 +0100

    Update ReadMe 11/11/22 Monti

---
+++ esercizio3.java
@@ -0,0 +1,33 @@
+class CiA{
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        //DICHIARAZIONE
+        double NumeroOne=0;
+        double NumeroTWO=0;
+        char Numero3='b';
+        double Somma=0;
+        boolean Bool1=true;
+        boolean Bool2=false;
+       boolean EVVIVA;
+        //valorizzazione
+        NumeroOne=123.34;
+        NumeroTWO=1;
+        Numero3='b';
+        //OPERAZIONI
+        Somma=NumeroOne + NumeroTWO+ Numero3;
+        Somma /= 3;
+        EVVIVA=(Bool1 == !Bool2);
+
+
+        //stampe
+        
+        System.out.println(Somma);
+
+        System.out.println(EVVIVA);
+        
+     
+
+}
+}
