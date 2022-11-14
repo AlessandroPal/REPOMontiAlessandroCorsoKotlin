969196aa9e724b706867b75914a7b40919214964
AlessandroPal <117976138+AlessandroPal@users.noreply.github.com>
2022-11-14 15:17:58 +0100

    Update ReadME 14/11/22 Monti
    Aggiunta esercizio cicli while e dowhile

---
+++ CiclieIteratori.java
@@ -0,0 +1,25 @@
+
+class CiclieIteratori{
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        int iProva=8;
+        while(iProva<9)
+        {
+          iProva++;
+          System.out.println(iProva);
+        
+        
+        
+        }
+       do{
+         iProva++;
+
+        System.out.println(iProva);
+        }while(iProva<9);
+        
+        } 
+     
+
+}
